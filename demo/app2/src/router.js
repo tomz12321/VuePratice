69b16dc8e33efe5c3ef3681341e4c29509801d0e
tomz123 <tomz12321@gmail.com>
2018-12-15 17:31:03 +0800

    Practice create new component
    Practice create new component

--- demo/app2/src/router.js
+++ demo/app2/src/router.js
@@ -21,5 +21,10 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
+    {
+      path: '/5xRuby',
+      name: '5xRuby',
+      component: () => import(/* webpackChunkName: "about" */ './views/5xRuby.vue')
+    },
   ]
 })
